25c5d62653cceb4f646f33bc7412b80df7c298c6
Okera Murray <murrayokera@gmail.com>
2021-09-19 20:32:02 -0400

    Part 2 Completed

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Okera Murray on 9/18/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
